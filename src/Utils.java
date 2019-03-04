f067f13df401518ceb428d9c9f850fc1120f0a46
David Zhang <dzdavid133@gmail.com>
2019-03-03 21:46:59 -0800

    created parse2016PresidentialResults() in Utils.java

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utils {
@@ -18,5 +19,15 @@ public class Utils {
         }
 
         return output.toString();
+    }
+
+    public static ArrayList<ElectionResult> parse2016PresidentalResults(String data) {
+        String[] lines = data.split("\n");
+        ArrayList<ElectionResult> output = new ArrayList<>(lines.length);
+        for(int i = 0; i < lines.length; i++) {
+            String[] resultData = lines[i].split(",");
+            output.add(new ElectionResult(Double.parseDouble(resultData[1]), Double.parseDouble(resultData[2]), Double.parseDouble(resultData[3]), Double.parseDouble(resultData[4]), Double.parseDouble(resultData[5]), resultData[6], Double.parseDouble(resultData[7].substring(0, resultData[7].length() - 1)), resultData[8], resultData[9], Integer.parseInt(resultData[10])));
+        }
+        return output;
     }
 }
